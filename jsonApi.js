5936a31efa270521021902372c1d0b2801056e70
Zkr Abbd <ZkrAbbd690@gmail.com>
2022-12-12 15:35:42 +0100

    Add existing project files to Git

--- jsonApi.js
+++ jsonApi.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var cors = require('cors'); // Use cors module for enable Cross-origin resource sharing
 var app = express();
+const url = require('url');
 app.use(cors()); // for all routes
 var port = process.env.PORT || 80 ;
 app.get('/', function(req, res) {
-var info = {
-'string_value': 'StackOverflow',
-'number_value': 8476
-}
-res.json(info);
+
+    res.redirect(url.format({
+        pathname:"noutee://app.noutee.com",
+        query:req.query
+      }));
+
+
+
+
+
+
+    //var info = {
+//'string_value': 'StackOverflow',
+//'number_value': 8476
+//}
+//res.json(info);
 // or
 /* res.send(JSON.stringify({
 string_value: 'StackOverflow',
